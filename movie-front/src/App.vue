d59d439fb16f5fd403d2ac000c936f6475c34221
blueboy1593 <snb0303@naver.com>
2019-11-25 09:37:35 +0900

    test2

--- movie-front/src/App.vue
+++ movie-front/src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <h1>JUNE'S MOVIE</h1>
       <h2>Pull Request 잘 되는지 확인</h2>
+      <h2>Pull Request 잘 되는지 확인2</h2>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
